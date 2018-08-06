504e7ee2000acb2c7359164071276143241b7bf2
Zachkery Rodriguez <39169752+ZVR999@users.noreply.github.com>
2018-08-06 17:50:19 -0500

    Answer and Guess
    Added theNumber variable to hold a random int at the start of the program (So this doesn't change when switching routes)
    Added method and methods to the from in index.html and server.py's '/process' route
    The player's guess is viable in terminal with no issues

--- server.py
+++ server.py
@@ -1,8 +1,18 @@
 from flask import Flask, redirect, render_template, session, request
+import random
+theNumber = random.randint(0,101)
 app = Flask(__name__)
 app.secret_key = 'ascnwoen?aijpnnwfwf3*^%$ac'
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    session['guess'] = request.form['guess']
+    guess = session['guess']
+    print guess
+    return redirect('/')
+
 app.run(debug=True)
