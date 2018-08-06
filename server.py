9920fc8b39975a278f69424dabcff8e0b32d291b
Zachkery Rodriguez <39169752+ZVR999@users.noreply.github.com>
2018-08-06 18:15:04 -0500

    Test response
    Added if/elif statements to the /process route. This is to see if while guessing the answer, will the correct response come up when submitted. After changing session['guess'] in an int type from a unicode type, no issues. Will change to variable to display on index.html rather than just the terminal

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect, render_template, session, request
 import random
 theNumber = random.randint(0,101)
 app = Flask(__name__)
-app.secret_key = 'ascnwoen?aijpnnwfwf3*^%$ac'
+app.secret_key = 'asc14$@en?aijpnnwfwf3*^%$ac'
 
 @app.route('/')
 def index():
@@ -11,8 +11,24 @@ def index():
 @app.route('/process', methods=['POST'])
 def process():
     session['guess'] = request.form['guess']
-    guess = session['guess']
+    guess = int(session['guess'])
     print guess
-    return redirect('/')
+    print theNumber
+    
+    session['response'] = ''
+    response = session['response']
+    
+    if guess > theNumber:
+        response = 'Too High!'
+        print response
+        return redirect('/')
+    elif guess < theNumber:
+        response = 'Too Low!'
+        print response
+        return redirect('/')
+    elif guess == theNumber:
+        response = str(theNumber)+' was the number!'
+        print response
+        return redirect('/')
 
 app.run(debug=True)
